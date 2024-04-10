e6fe43c3a52a36ab298a688408db5b4793eccefe
Abdelrahman <abdelrahmanhamada65@gmail.com>
2024-04-10 03:49:28 +0200

    basic template

--- backend/wsgi_app.py
+++ backend/wsgi_app.py
@@ -2,15 +2,23 @@ from client.main_view import View
 
 client_view = View()
 
-def wsgi_application(environ, start_resonse):
+def open_html(view):
+    template = view.template
+    with open(f"client/{template}", "rb") as html:
+        encoded_data = html.read()
+
+    return encoded_data
+
+def wsgi_application(environ, start_response):
     headers = [('Content-type', 'text/html')]
     try:
-        body = client_view.get_body().encode("utf-8")
+        body = open_html(client_view)
         status = "200 OK"
         client_view.flag = False
-    except Exception:
+    except Exception as e:
+        print(e)
         body = b"Error"
         status = "404 Not Found"
 
-    start_resonse(status, headers)
+    start_response(status, headers)
     return [body]
--- client/main_view.py
+++ client/main_view.py
@@ -1,11 +1,3 @@
-flag = True
-
 class View():
     flag = True
-
-    def get_body(self):
-        # Solely for testing exceptions
-        if not self.flag:
-            raise Exception
-
-        return "<h1>Hello from the client side</h1>"
+    template = "templates/test.html"
